8bb676941a4863382d84f57adbb5792b12c20115
yuanguandong <995749721@qq.com>
2021-07-29 05:22:58 +0000

    Automated build docs

--- docs/sw.js
+++ docs/sw.js
@@ -27,39 +27,42 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8f3c274bd3fbb0d48f2d.js"
+    "url": "webpack-runtime-b875085bccf61c474b73.js"
   },
   {
     "url": "styles.22aa8ee0c2e6efe836b5.css"
   },
   {
-    "url": "styles-e9d24b1846c7d6eb9685.js"
+    "url": "styles-7d4153d260c0197f0043.js"
   },
   {
-    "url": "framework-beda7bde9d7dca0755ce.js"
+    "url": "framework-acd7498685eeb36e39da.js"
   },
   {
-    "url": "app-ce554a8b8034a974df24.js"
+    "url": "app-1a55be347b491e648cb2.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "94dfa8051296099d48a0174905b6d679"
+    "revision": "e10301a1c68f8034a87192de62572fc3"
   },
   {
-    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-fd4fb51a6fac1c18bdde.js"
+    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-a95b272f8c25b9a8a758.js"
   },
   {
-    "url": "polyfill-d32d537923e12a484928.js"
+    "url": "polyfill-2fa4791f5d87adda3b2a.js"
   },
   {
-    "url": "22df2c6f0e0b02f286186edbe62a3193fb639611-b0587a98d74a87b3beff.js"
+    "url": "1bfc9850-2971a235dcba394cb3cc.js"
   },
   {
-    "url": "component---src-pages-404-js-9d55a682f957f2a44047.js"
+    "url": "94158ac8cbf2b7d02a956c35d3a10ee3a2fed2cd-7a5ae92101046d663681.js"
+  },
+  {
+    "url": "component---src-pages-404-js-7f93dfb86a77dff33ee9.js"
   },
   {
     "url": "page-data/404/page-data.json",
-    "revision": "0444920d988b0fba30285f2640d4be2e"
+    "revision": "634b3e0b6621da7a7a7828fc7dc28746"
   },
   {
     "url": "page-data/sq/d/1143375668.json",
@@ -74,25 +77,29 @@ self.__precacheManifest = [
     "revision": "50a29123b31812be71b55a128f1f52c9"
   },
   {
+    "url": "page-data/sq/d/3802270912.json",
+    "revision": "9de6086766061a83e06d4d522a00a9fe"
+  },
+  {
     "url": "page-data/app-data.json",
-    "revision": "8f7594097aa37622257dfe3ff3c2818d"
+    "revision": "714b09731036a14779682cee3d8cd092"
   },
   {
     "url": "page-data/404.html/page-data.json",
-    "revision": "36ead6c429c4f0e4f0493ad4ab18d1c1"
+    "revision": "1d6cdf2b3e0ef5ab52e9cdfae570f833"
   },
   {
-    "url": "f0e45107-042b081a9beedf37ad59.js"
+    "url": "f0e45107-4690c1fcd75a276689d2.js"
   },
   {
-    "url": "f7f859cd4bf156c2acc6f46f65ba39cb35d4c0a9-8e2b10e123f08132686b.js"
+    "url": "d59818a277a63e94f3957e3702ab9a81b507562b-5a86ce10babf93d999fe.js"
   },
   {
-    "url": "component---node-modules-narative-gatsby-theme-novela-src-templates-article-template-tsx-c4b5ec189faf95e0023a.js"
+    "url": "component---src-narative-gatsby-theme-novela-templates-article-template-tsx-5865e4a70bd2ca3fd1d7.js"
   },
   {
     "url": "page-data/ajax/page-data.json",
-    "revision": "e58972cb136046e5c61cc9c99d792d5b"
+    "revision": "8de819cbf4db3e898db0fea57ae37742"
   },
   {
     "url": "page-data/sq/d/1921650733.json",
@@ -104,60 +111,60 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/callapplybind/page-data.json",
-    "revision": "7cc0e1843bebb5d88ca8eb4fc0cc179b"
+    "revision": "1dac5e101cfe3aa351e208589958322b"
   },
   {
-    "url": "page-data/cssyuan/page-data.json",
-    "revision": "194bf7ff1bec816b260a83335a97d41e"
+    "url": "page-data/dashboard1/page-data.json",
+    "revision": "373acf935cea0802ab282d45545c28f5"
   },
   {
-    "url": "page-data/dashboard/page-data.json",
-    "revision": "f327e2136f4d7f5e1968a0383cc96bdb"
+    "url": "page-data/debounce/page-data.json",
+    "revision": "860fb37335fb5ae01b4fcf1670b7b81e"
   },
   {
     "url": "page-data/fiber/page-data.json",
-    "revision": "c22a62821ccf5e6d852f00b13f51b9f5"
+    "revision": "8192ac72f77583c7dfa054d5ad8518d8"
   },
   {
-    "url": "cfc86df3-70787a6ec6fec8018aa6.js"
+    "url": "9d86c5a1-2ccb1a1443015975124b.js"
   },
   {
-    "url": "5adfd937054d18aa90c3653907d1d43da6549296-bc50838dcf99e46772bb.js"
+    "url": "5fa2fdbf4b7a894b8ac41185861b18ed815ef969-702f37162f5334b3ee27.js"
   },
   {
-    "url": "component---node-modules-narative-gatsby-theme-novela-src-templates-articles-template-tsx-d9850c23be282e563237.js"
+    "url": "component---src-narative-gatsby-theme-novela-templates-articles-template-tsx-c1f68ffb03dd8565c573.js"
   },
   {
     "url": "page-data/index/page-data.json",
-    "revision": "3b2b87bbeb10dffdbdc6f732d783cb27"
+    "revision": "0b82b9429673b8b1432fef5f95d75991"
   },
   {
     "url": "page-data/sq/d/3372861117.json",
     "revision": "0964a287b783a7fdcdc56dfb85739139"
   },
   {
-    "url": "page-data/js/page-data.json",
-    "revision": "8fc8b2c7adba7a3466fed19f0d06a51a"
+    "url": "page-data/jsbackground/page-data.json",
+    "revision": "2f10b7144ebf18ec134c07084799ce32"
   },
   {
-    "url": "page-data/jscss-'background:paint(xxx)'/page-data.json",
-    "revision": "bdb1fc532c2ed28e13afd58d29ea9d11"
+    "url": "page-data/liststyle/page-data.json",
+    "revision": "55b1a01903e4eb58162e17b76983cf15"
+  },
+  {
+    "url": "page-data/module/page-data.json",
+    "revision": "02872a177ae5a96ec6ed03e2a07109a5"
   },
   {
     "url": "page-data/promisea+/page-data.json",
-    "revision": "e94a41da758a1e1b65dc7c02905ece4e"
+    "revision": "fa1ff5e7aadf335636acdfeaeee8ea97"
   },
   {
-    "url": "page-data/throttledebounce/page-data.json",
-    "revision": "ab5d6b7faca3851068c90bff0be75782"
-  },
-  {
-    "url": "page-data/typescript/page-data.json",
-    "revision": "299eecc72efccfaa24cfb5f6f7801f48"
+    "url": "page-data/tsbase/page-data.json",
+    "revision": "d40ee566d4514589f7f9dd3efa0f7a80"
   },
   {
     "url": "page-data/usestate/page-data.json",
-    "revision": "8d6ff49c19a5651fe0d8d3d516bb86f3"
+    "revision": "84edae6b251d4e3ea75df963e7b08644"
   },
   {
     "url": "manifest.webmanifest",
@@ -253,7 +260,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-ce554a8b8034a974df24.js`))) {
+  if (!resources || !(await caches.match(`/app-1a55be347b491e648cb2.js`))) {
     return await fetch(event.request)
   }
 
