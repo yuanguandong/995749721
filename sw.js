fb0a9a1b20fbb1ae5d058b7f41ea859de2a2f8b1
yuanguandong <yuanguandong@users.noreply.github.com>
2021-08-02 04:28:19 +0000

    deploy: 32ae621797227b2912539bd4fef3041f5d904854

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-bc900d363c55c353a77b.js"
+    "url": "webpack-runtime-a20f7f4d79860c9c8b43.js"
   },
   {
     "url": "styles.a04ff0508dc5a70456b8.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7628700798860b9e237d47856731ddbc"
+    "revision": "bc0d57698ce1955c49a4b17aea9a6afc"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-77466cc0b3ed4dac08eb.js"
