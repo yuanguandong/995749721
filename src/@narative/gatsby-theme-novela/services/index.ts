32ae621797227b2912539bd4fef3041f5d904854
yuanguandong <995749721@qq.com>
2021-08-02 12:25:03 +0800

    feat: add motto

--- src/@narative/gatsby-theme-novela/services/index.ts
+++ src/@narative/gatsby-theme-novela/services/index.ts
@@ -28,7 +28,7 @@ const data = [
   {
     author: "庄子",
     content:
-      "吾生也有涯，而知也无涯。以在涯随无涯，殆已；已而为知者，殆而已矣。",
+      "吾生也有涯，而知也无涯。以有涯随无涯, 殆已；已而为知者，殆而已矣。",
   },
   {
     author: "佚名",
@@ -68,7 +68,11 @@ const data = [
   },
   {
     author: "乔布斯",
-    content: "最重要的是，勇敢的去追随自己的心灵和直觉，只有自己的心灵和直觉才知道你自己的真实想法，其他一切都是次要。",
+    content: "最重要的是，勇敢的去追随自己的心灵和直觉。",
+  },
+  {
+    author: "乔布斯",
+    content: "只有自己的心灵和直觉才知道你自己的真实想法，其他一切都是次要。",
   },
   {
     author: "乔布斯",
@@ -80,14 +84,209 @@ const data = [
   },
   {
     author: "乔布斯",
-    content: "牢记自己即将死去，这是避免陷入患得患失困境的最好方法。一无所有，就没有理由不听从自己的心声",
+    content: "牢记自己即将死去，这是避免陷入患得患失困境的最好方法。",
   },
   {
     author: "诸葛亮",
     content: "善战者不怒，善胜者不惧。",
   },
-  
-  
+  {
+    author: "韩愈",
+    content: "业精于勤，荒于嬉；行成于思，毁于随。",
+  },
+  {
+    author: "欧阳修",
+    content: "祸患常积于忽微，而智勇多困于所溺。",
+  },
+  {
+    author: "屈原",
+    content: "亦余心之所善兮,虽九死其犹未悔。",
+  },
+  {
+    author: "孔子",
+    content: "岁寒，然后知松柏之后凋也。",
+  },
+  {
+    author: "陶渊明",
+    content: "盛年不重来，一日难再晨，及时当勉励，岁月不待人。",
+  },
+  {
+    author: "彭端叔",
+    content: "天下事有难易乎，为之，则难者亦易矣；不为，则易者亦难矣。",
+  },
+  {
+    author: "孔子",
+    content: "士不可不弘毅，任重而道远。",
+  },
+  {
+    author: "鲍照",
+    content: "时危见臣节，世乱识忠良。",
+  },
+  {
+    author: "鲁迅",
+    content: "心事浩茫连广宇，于无声处听惊雷。",
+  },
+  {
+    author: "苟子",
+    content: "天行有常，不为尧存，不为桀亡。",
+  },
+  {
+    author: "诗经·小雅",
+    content: "它山之石，可以攻玉。",
+  },
+  {
+    author: "林逋",
+    content: "疏影横斜水清浅，暗香浮动月黄昏。",
+  },
+  {
+    author: "诗经·小雅",
+    content: "它山之石，可以攻玉。",
+  },
+  {
+    author: "荀子",
+    content: "岁不寒，无以知松柏；事不难，无以知君子。",
+  },
+  {
+    author: "孔子",
+    content: "知之者不如好之者，好之者不如乐之者。",
+  },
+  {
+    author: "荀子",
+    content: "无边落木萧萧下，不尽长江滚滚来。",
+  },
+  {
+    author: "墨子·修身",
+    content: "志不强者智不达。",
+  },
+  {
+    author: "苏轼",
+    content: "回首向来萧瑟处，归去， 也无风雨也无晴。",
+  },
+  {
+    author: "王勃",
+    content: "飞阁流丹，下临无地。",
+  },
+  {
+    author: "孟子",
+    content: "一日暴之，十日寒之，未有能生者也。",
+  },
+  {
+    author: "留侯论",
+    content: "天下有大勇者，卒然临之而不惊，无故加之而不怒。",
+  },
+  {
+    author: "未知",
+    content: "大风起兮云飞扬，威加海内兮归故乡，安得猛士兮守四方！",
+  },
+  {
+    author: "王勃",
+    content: "穷且益坚，不坠青云之志。",
+  },
+  {
+    author: "前赤壁赋",
+    content: "浩浩乎如冯虚御风，而不知其所止；飘飘乎如遗世独立，羽化而登仙。",
+  },
+  {
+    author: "苏轼",
+    content: "竹杖芒鞋轻胜马，谁怕？一蓑烟雨任平生。",
+  },
+  {
+    author: "孟子·告子下",
+    content: "故天将降大任于斯人也，必先苦其心志，劳其筋骨，饿其体肤……",
+  },
+  {
+    author: "把酒问月",
+    content: "今人不见古时月，今月曾经照古人。",
+  },
+  {
+    author: "苏轼",
+    content: "持节云中，何日遣冯唐？会挽雕弓如满月，西北望，射天狼。",
+  },
+  {
+    author: "庄周",
+    content: "北冥有鱼，其名为鲲。鲲之大，不知其几千里也",
+  },
+  {
+    author: "庄周",
+    content: "鹏之徙于南冥也，水击三千里，抟扶摇而上者九万里，去以六月息者也。",
+  },
+  {
+    author: "庄周",
+    content: "且夫水之积也不厚，则其负大舟也无力。",
+  },
+  {
+    author: "庄周",
+    content: "小知不及大知，小年不及大年。奚以知其然也？",
+  },
+  {
+    author: "庄周",
+    content: "有鸟焉，其名为鹏，背若泰山，翼若垂天之云，抟扶摇羊角而上者九万里，绝云气，负青天。",
+  },
+  {
+    author: "庄周",
+    content: "若夫乘天地之正，而御六气之辩，以游无穷者，彼且恶乎待哉？",
+  },
+  {
+    author: "庄周",
+    content: "至人无己，神人无功，圣人无名。",
+  },
+  {
+    author: "谚语",
+    content: "Nothing in the world is difficult for one who sets his mind to it.",
+  },
+  {
+    author: "谚语",
+    content: "Nothing is impossible to a willing heart.",
+  }, {
+    author: "谚语",
+    content: "Where there is a will, there is a way.",
+  }, {
+    author: "谚语",
+    content: "Confidence in yourself is the first step on the road to success.",
+  }, {
+    author: "谚语",
+    content: "Fools learn nothing from wise men, but wise men learn much from fools. ",
+  }, {
+    author: "谚语",
+    content: "There is no end to learning.",
+  }, {
+    author: "谚语",
+    content: "Study to be what you wish to seem. ",
+  }, {
+    author: "谚语",
+    content: "Knowledge starts with practice.",
+  }, {
+    author: "谚语",
+    content: "Complacency is the enemy of study.",
+  }, {
+    author: "谚语",
+    content: "A young idler, an old beggar. ",
+  }, {
+    author: "谚语",
+    content: "A good beginning is half done.",
+  }, {
+    author: "谚语",
+    content: "Let’s do it by ourselves.",
+  }, {
+    author: "谚语",
+    content: "Time is life.",
+  }, {
+    author: "谚语",
+    content: "if you can't beat them , join them .",
+  }, {
+    author: "谚语",
+    content: "The fire is the test of gold; adversity of strong man.",
+  }, {
+    author: "谚语",
+    content: "Victory won't come to me unless I go to it.",
+  }, {
+    author: "谚语",
+    content: "Cease to struggle and you cease to live.",
+  }, {
+    author: "谚语",
+    content: "A thousand-li journey is started by taking the first step.",
+  }, {
+    author: "谚语",
+    content: "Never underestimate your power to change yourself!",
+  }
 ];
-
-
