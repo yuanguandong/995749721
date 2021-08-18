65868c0b1aca6305607266c2d15d11e54ef6c4d4
yuanguandong <yuanguandong@users.noreply.github.com>
2021-08-18 04:48:16 +0000

    deploy: a36c88051c0dfa777cc24a33a6112bd0f6dfac43

--- music.ts
+++ music.ts
@@ -49,5 +49,35 @@ export default [
     id: "18209568",
     author: "Hans Zimmer",
   },
+  {
+    name: "Angels Will Rise",
+    id: "29844298",
+    author: "Twisted Jukebo",
+  },
+  {
+    name: "When It All Falls Down",
+    id: "32835870",
+    author: "Audiomachine",
+  },
+  {
+    name: "Icarus",
+    id: "29823210",
+    author: "Ivan Torrent",
+  },
+  {
+    name: "Star Sky - Instrumental",
+    id: "31654479",
+    author: "Two Steps From Hell",
+  },
+  {
+    name: "亡灵序曲",
+    id: "31654479",
+    author: "L",
+  },
+  {
+    name: "Up Is Down",
+    id: "1427888",
+    author: "Hans Zimm",
+  },
 
 ];
