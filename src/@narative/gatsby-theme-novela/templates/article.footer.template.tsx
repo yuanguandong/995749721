d2476c29547d14db93af2ce52a6d894ea2ea6cd6
yuanguandong <yuangd@gantsoftware.com>
2022-07-18 00:23:22 +0800

    chore: udpate

--- src/@narative/gatsby-theme-novela/templates/article.footer.template.tsx
+++ src/@narative/gatsby-theme-novela/templates/article.footer.template.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 // import "gitalk/dist/gitalk.css";
-import GitalkComponent from "gitalk/dist/gitalk-component";
-import "./gitalk.css"
+// import GitalkComponent from "gitalk/dist/gitalk-component";
+// import "./gitalk.css"
 // import Subscription from "@components/Subscription";
 
 // import { Template } from "@types";
 
 const ArticleFooter = ({ pageContext }) => {
   const { article, mailchimp } = pageContext;
-  console.log("article", article);
+  // console.log("article", article);
   return (
     <>
-      <GitalkComponent
+      {/* <GitalkComponent
         options={{
           clientID: "9a1d1d842995b8bd8f79",
           clientSecret: "6fda9f068656d0916efe20f3bcb6034a85548b58",
@@ -20,7 +20,7 @@ const ArticleFooter = ({ pageContext }) => {
           admin: ["yuanguandong"],
           id: article?.slug,
         }}
-      />
+      /> */}
     </>
   );
 };
