80fb3e48eb291d12671564aabb8372474597847d
yuanguandong <yuangd@gantsoftware.com>
2022-07-18 00:01:19 +0800

    chore: update

--- src/@narative/gatsby-theme-novela/templates/article.footer.template.tsx
+++ src/@narative/gatsby-theme-novela/templates/article.footer.template.tsx
@@ -1,28 +1,30 @@
 import React from "react";
 // import "gitalk/dist/gitalk.css";
-// import GitalkComponent from "gitalk/dist/gitalk-component";
+import GitalkComponent from "gitalk/dist/gitalk-component";
+import "./gitalk.css"
 // import Subscription from "@components/Subscription";
 
 // import { Template } from "@types";
 
 const ArticleFooter = ({ pageContext }) => {
   const { article, mailchimp } = pageContext;
-  // console.log("article", article);
+  console.log("article", article);
   return (
     <>
-      {/* {mailchimp && article.subscription && <Subscription />} */}
-      {/* <GitalkComponent
+      <GitalkComponent
         options={{
           clientID: "9a1d1d842995b8bd8f79",
-          clientSecret:"6fda9f068656d0916efe20f3bcb6034a85548b58",
-          repo:"https://github.com/yuanguandong/blog-comments",
-          owner:'yuanguandong',
-          admin:'yuanguandong',
-          id:''
+          clientSecret: "6fda9f068656d0916efe20f3bcb6034a85548b58",
+          repo: "blog-comments",
+          owner: "yuanguandong",
+          admin: ["yuanguandong"],
+          id: article?.slug,
         }}
-      /> */}
+      />
     </>
   );
 };
+
+/* {mailchimp && article.subscription && <Subscription />} */
 
 export default ArticleFooter;
